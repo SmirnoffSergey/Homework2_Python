749d0a5efe92a730fdcc8eec6f1e5b637f5ccfb5
Sergey <s_smirnov@nisse.ru>
2022-09-28 13:47:09 +0300

    fixed Ex3.py

--- Ex3.py
+++ Ex3.py
@@ -3,10 +3,10 @@
 
 n = int(input('Enter the quantity of numbers in list n: '))
 sum = 0
-j = 0
+number_of_sequence = 0
 for i in range(1, n+1):
-    j = (1 + 1/i)**i
-    print(round(j, 2))
+    number_of_sequence = (1 + 1/i)**i
+    print(f'The {i} number of sequence is {round(number_of_sequence, 2)}')
     sum += (1 + 1/i)**i
 print()
 print(f'The sum of the numbers of the sequence (1+1/n)^n is {round(sum, 2)}')
